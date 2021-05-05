83cdba5f2dc1d0f6cd4c2f2a896477281759ab4c
Roberto Rossi <robertorossi.arg@gmail.com>
2021-05-05 09:58:42 -0300

    CP commiteado por robertorossi-arg a las 1620219522919

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -36,6 +36,9 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+
+  for(let i=0; i<array.length; i++)
+      cb(array[i]);
 }
 
 function map(array, cb) {
@@ -43,12 +46,23 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nuevo =  [];
+  for (i=0; i<array.length; i++)
+    nuevo.push(cb(array[i]));
+
+  return nuevo;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var nuevo=[];
+  for (var i=0; i<array.length; i++)
+    if (array[i][0] === 'a')
+      nuevo.push(array[i]);
+
+  return nuevo;
 }
 
 // No modificar nada debajo de esta línea
